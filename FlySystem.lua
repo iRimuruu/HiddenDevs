7a4142badfd358ecb65a499edf44a27b262e3090
iRimuruu <149233658+iRimuruu@users.noreply.github.com>
2026-08-30 01:44:52 -0300

    Update FlySystem.lua

--- FlySystem.lua
+++ FlySystem.lua
@@ -1,8 +1,5 @@
 --[[
-I'd like to note that in this system I use some legacy instances, simply because I prefer them and find the end result better; I'm proficient with LinearVelocity and others.
-Credits in game description
-
-https://www.roblox.com/pt/games/136795959725853/FlyingTest
+-- Roblox = JustyNeru                  Discord = nep00143
 ]]
 
 --\\Modules
@@ -19,6 +16,8 @@ local char:Model = script.Parent -- getting the player char
 local HMD:Humanoid = char:WaitForChild("Humanoid") -- getting humanoid
 local RootPart:BasePart = char:WaitForChild("HumanoidRootPart") -- Getting HumanoidRootPart
 
+local Forces:Attachment = RootPart:FindFirstChild("Forces") -- Getting Forces attachment
+
 local Camera:Camera = workspace.CurrentCamera  -- Getting Camera
 
 --\\ Configs
@@ -28,8 +27,8 @@ local AnimsRemote = game.ReplicatedStorage.Remote -- Getting the remote to activ
 local Configs = {
 	IsFlying = false, -- To find out if your flight is active or not.
 	
-	BodyVelocity = nil, --Body velocity object
-	BodyGyroX = nil, --Body velocity object
+	LinearVelocity = nil, --LinearVelocity Object
+	AlignOrientation = nil, --AlignOrientation Object
 	
 	--\\ Movement_Configs
 	
@@ -67,30 +66,44 @@ local Configs = {
 --\\ Functions
 
 local function TurnFly() -- Function to activate or deactivate the fly
-	if not Configs.IsFlying and not Configs.BodyVelocity then -- I check if the player is not flying and if there is a BodyVelocity object already created to be sure.
+	if not Configs.IsFlying and not Configs.LinearVelocity then -- I check if the player is not flying and if there is a BodyVelocity object already created to be sure.
 		
 		print("Flying")
 		
 		Configs.IsFlying = true --I changed the flying status to true.
-		Configs.BodyVelocity = Instance.new("BodyVelocity") -- I create a new bodyVelocity
-		Configs.BodyVelocity.MaxForce = Vector3.new(math.huge, math.huge, math.huge) -- I define his strength as math.huge.
-		Configs.BodyVelocity.Parent = RootPart -- I put it inside RootPart.
 		
-		Configs.BodyGyroX = Instance.new("BodyGyro") -- I create a new bodyGyro
-		Configs.BodyGyroX.Parent = RootPart -- I put it inside RootPart.
-		Configs.BodyGyroX.MaxTorque = Vector3.new(math.huge, math.huge, math.huge) -- I define his torque as math.huge.
-		Configs.BodyGyroX.Name = "GyroX" -- I name Gyro
-		Configs.BodyGyroX.P = 10000 -- Increase the Gyro's strength
+		-- LinearVelocity SETUP
 		
-	elseif Configs.IsFlying and Configs.BodyVelocity then -- checks if it is already flying and if a bodyVelocity has already been created.
+		local LinearVelocity = Instance.new("LinearVelocity", Forces) -- I Create the LinearVelocity instance and put inside Forces attachment
+		
+		LinearVelocity.Attachment0 = Forces -- i setup the Attachment0
+		
+		LinearVelocity.ForceLimitsEnabled = false -- I disable the roblox's forces to prevent them from interfering
+		 
+		Configs.LinearVelocity = LinearVelocity -- Put on configs table
+		
+		--AlignOrientation SETUP
+		
+		local AlignOrientation = Instance.new("AlignOrientation", Forces)  -- I Create the AlignOrientation instance  and put inside Forces attachment
+		
+		AlignOrientation.Mode = Enum.OrientationAlignmentMode.OneAttachment -- i Setup the Alignment Mode to One Attachment
+		AlignOrientation.Attachment0 = Forces -- i setup the Attachment0
+		
+		AlignOrientation.MaxTorque = math.huge
+		AlignOrientation.Responsiveness = 100
+		
+		Configs.AlignOrientation = AlignOrientation -- Put on configs table
+		
+		
+	elseif Configs.IsFlying and Configs.LinearVelocity then -- checks if it is already flying and if a bodyVelocity has already been created.
 		
 		print("Not Flying")
 		
 		Configs.IsFlying = false -- I changed the flying status to false.
-		Configs.BodyVelocity:Destroy() -- Destroys bodyVelocity
-		Configs.BodyVelocity = nil -- Remove the reference to it.
-		Configs.BodyGyroX:Destroy() -- Destroys bodyGyro
-		Configs.BodyGyroX = nil-- Remove the reference to it.
+		Configs.LinearVelocity:Destroy() -- Destroys LinearVelocity
+		Configs.LinearVelocity = nil -- Remove the reference to it.
+		Configs.AlignOrientation:Destroy() -- Destroys AlignOrientation
+		Configs.AlignOrientation = nil-- Remove the reference to it.
 	end
 end
 
@@ -131,23 +144,23 @@ end
 local MovementFunctions = { -- -- All functions for each movement
 	Idle = function()
 		ResetMovement() -- always keeps the Alphas at 0
-		Configs.BodyVelocity.Velocity = Vector3.zero -- Set the speed to zero.
+		Configs.LinearVelocity.VectorVelocity = Vector3.zero -- Set the speed to zero.
 	end,
 	Front = function(Vector: Vector3, dt: number) -- Moving forward, it receives the current LookVector and the deltaTime to lerp
 		
-		local bv:BodyVelocity = Configs.BodyVelocity -- Reference BodyVelocity for ease of use.
+		local lv:LinearVelocity= Configs.LinearVelocity -- Reference BodyVelocity for ease of use.
 		
 		Configs.FMovementAlpha += dt * Configs.FSpeedMultiplier -- calculation to increase lerp
 		
 		local finalAlpha = math.clamp(Configs.FMovementAlpha, 0, 1) -- To ensure that alpha never goes above 0 or 1
 		
-		local finalVelocity = bv.Velocity:Lerp(Vector * Configs.FMaxVelocity, finalAlpha) -- Calculate the target velocity using lerp, taking the current lookvector and multiplying it by the speed multiplier.
+		local finalVelocity = lv.VectorVelocity:Lerp(Vector * Configs.FMaxVelocity, finalAlpha) -- Calculate the target velocity using lerp, taking the current lookvector and multiplying it by the speed multiplier.
 		
-		bv.Velocity = finalVelocity -- Place the result of lerp in the velocity of BodyVelocity.
+	 lv.VectorVelocity = finalVelocity -- Place the result of lerp in the velocity of BodyVelocity.
 	end,
 	
 	Backwards = function(Vector: Vector3, dt: number) -- Moving backWards, it receives the current LookVector and the deltaTime to lerp
-		local bv:BodyVelocity = Configs.BodyVelocity -- Reference BodyVelocity for ease of use.
+		local lv:LinearVelocity = Configs.LinearVelocity -- Reference BodyVelocity for ease of use.
 		
 		local BackVector = Vector * -1 -- I transform the LookVector into BackVector by making it negative
 		
@@ -155,30 +168,30 @@ local MovementFunctions = { -- -- All functions for each movement
 
 		local finalAlpha = math.clamp(Configs.BMovementAlpha, 0, 1) -- To ensure that alpha never goes above 0 or 1
 
-		local finalVelocity = bv.Velocity:Lerp(BackVector * Configs.BMaxVelocity, finalAlpha) -- Calculate the target velocity using lerp, taking the current BackVector and multiplying it by the speed multiplier.
+		local finalVelocity = lv.VectorVelocity:Lerp(BackVector * Configs.BMaxVelocity, finalAlpha) -- Calculate the target velocity using lerp, taking the current BackVector and multiplying it by the speed multiplier.
 
-		bv.Velocity = finalVelocity -- Place the result of lerp in the velocity of BodyVelocity.
+		lv.VectorVelocity = finalVelocity -- Place the result of lerp in the velocity of BodyVelocity.
 	end,
 	Right = function(Vector: Vector3, dt: number) -- Moving Right, it receives the current RightVector and the deltaTime to lerp
-		local bv:BodyVelocity = Configs.BodyVelocity -- Reference BodyVelocity for ease of use.
+		local lv:LinearVelocity = Configs.LinearVelocity -- Reference BodyVelocity for ease of use.
 		Configs.RMovementAlpha += dt * Configs.RSpeedMultiplier  -- calculation to increase lerp
 
 		local finalAlpha = math.clamp(Configs.RMovementAlpha, 0, 1) -- To ensure that alpha never goes above 0 or 1
 
-		local finalVelocity = bv.Velocity:Lerp(Vector * Configs.RMaxVelocity, finalAlpha) -- Calculate the target velocity using lerp, taking the current RightVector and multiplying it by the speed multiplier.
+		local finalVelocity = lv.VectorVelocity:Lerp(Vector * Configs.RMaxVelocity, finalAlpha) -- Calculate the target velocity using lerp, taking the current RightVector and multiplying it by the speed multiplier.
 
-		bv.Velocity = finalVelocity -- Place the result of lerp in the velocity of BodyVelocity.
+		lv.VectorVelocity = finalVelocity -- Place the result of lerp in the velocity of BodyVelocity.
 	end,
 	Left = function(Vector: Vector3, dt: number) -- Moving Left, it receives the current RightVecrto and the deltaTime to lerp
-		local bv:BodyVelocity = Configs.BodyVelocity -- Reference BodyVelocity for ease of use.
+		local lv:LinearVelocity = Configs.LinearVelocity -- Reference BodyVelocity for ease of use.
 		local LeftVector = Vector * -1 -- I transform the RightVector into LeftVector by making it negative
 		Configs.LMovementAlpha += dt * Configs.LSpeedMultiplier  -- calculation to increase lerp
 
 		local finalAlpha = math.clamp(Configs.LMovementAlpha, 0, 1) -- To ensure that alpha never goes above 0 or 1
 
-		local finalVelocity = bv.Velocity:Lerp(LeftVector * Configs.LMaxVelocity, finalAlpha) -- Calculate the target velocity using lerp, taking the current lookvector and multiplying it by the motion multiplier.
+		local finalVelocity = lv.VectorVelocity:Lerp(LeftVector * Configs.LMaxVelocity, finalAlpha) -- Calculate the target velocity using lerp, taking the current lookvector and multiplying it by the motion multiplier.
 
-		bv.Velocity = finalVelocity -- Place the result of lerp in the velocity of BodyVelocity.
+		lv.VectorVelocity = finalVelocity -- Place the result of lerp in the velocity of BodyVelocity.
 	end,
 }
 
@@ -190,11 +203,13 @@ UIS.InputBegan:Connect(function(input: InputObject, gameProcessedEvent: boolean)
 	end
 	
 	
-	if input.KeyCode == Enum.KeyCode.Space and HMD:GetState() == Enum.HumanoidStateType.Freefall then -- If his input is a space and he is in FreeFall, then I will use the turnFly function.
+	if input.KeyCode == Enum.KeyCode.Space and not Configs.IsFlying and HMD:GetState() == Enum.HumanoidStateType.Freefall then -- If his input is a space and he is in FreeFall, then I will use the turnFly function.
 		TurnFly() -- Depending on whether the player is already flying or not, they can disable or enable fly.
-		if not Configs.IsFlying then
-			AnimsRemote:Fire(nil, "Stop")-- If it stops flying, the animation is Stopped.
-		end
+		
+	elseif  input.KeyCode == Enum.KeyCode.Space and Configs.IsFlying then
+		
+		TurnFly() -- Depending on whether the player is already flying or not, they can disable or enable fly.
+		AnimsRemote:Fire(nil, "Stop")-- If it stops flying, the animation is Stopped.
 	end
 end)
 
@@ -210,7 +225,7 @@ RunService.PreRender:Connect(function(deltaTime: number) -- the main code
 	
 	
 	if Configs.IsFlying then -- Check if it's flying for the rest of the code to happen.
-		local GyroX:BodyGyro = Configs.BodyGyroX -- References bodyGyro for ease of use
+		local Align:AlignOrientation = Configs.AlignOrientation -- References bodyGyro for ease of use
 		
 		if not Configs.LastCameraOrientation then--  If there is no last orientation, it initializes.
 			Configs.LastCameraOrientation = Camera.CFrame.LookVector -- I set it as LookVector, since the character is always looking in the same direction as the camera during flight.
@@ -228,7 +243,7 @@ RunService.PreRender:Connect(function(deltaTime: number) -- the main code
 		
 		Configs.CurrentRoll += (Configs.TargetRoll - Configs.CurrentRoll) * Alpha -- Instead of using Lerp, I prefer to perform a calculation to continuously retrieve the TargetRoll - CurrentRoll * Alpha, updating it little by little until it reaches the target.
 		
-		GyroX.CFrame = Camera.CFrame * CFrame.Angles(0, 0, math.rad(Configs.CurrentRoll)) -- I apply the roll directly to the Gyro. And at the same time I update it to look directly in the direction of the camera.
+		Align.CFrame = Camera.CFrame * CFrame.Angles(0, 0, math.rad(Configs.CurrentRoll)) -- I apply the roll directly to the Gyro. And at the same time I update it to look directly in the direction of the camera.
 		
 		print("TargetRoll:", Configs.TargetRoll, "| CurrentRoll:", Configs.CurrentRoll) -- For debugging purposes, to check if the roll is being calculated correctly.
 		
